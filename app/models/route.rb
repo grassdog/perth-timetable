7e53d2d127bb569a1e7794abb6625d2b1accbdac
Tobias Lauchenauer <tobias@lauchenauer.info>
2012-09-26 10:55:48 +0800

    adjusted routes outputs

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,18 +3,31 @@ class Route < ActiveRecord::Base
   has_many :trips
 
   def self.get_all_routes_for_stop stop_number
-      connection = ActiveRecord::Base.connection()
-      result = connection.execute("SELECT DISTINCT
-                                    r.short_name as route_number,
-                                    r.long_name as route_name,
-                                    t.headsign
-                                    FROM services s
-                                    JOIN trips t ON t.service_id = s.service_id
-                                    JOIN routes r ON r.route_id = t.route_id
-                                    JOIN stop_times stopt ON stopt.trip_id = t.trip_id
-                                    JOIN stops stop ON stop.stop_id = stopt.stop_id
-                                    WHERE stop.stop_id = #{stop_number}
-                                    ORDER BY r.short_name")
-      { :routes => result.to_a }
+    stop = Stop.find_by_stop_id stop_number
+    stop_numbers = [stop_number] if stop.child_stops.empty?
+    stop_numbers = stop.child_stops.map { |s| s.stop_id } unless stop.child_stops.empty?
+
+    routes = []
+    stop_numbers.each do |s|
+      routes += get_routes_for_stop_with_no_children(s)
     end
+    { :routes => routes }
+  end
+
+  def self.get_routes_for_stop_with_no_children stop_number
+    connection = ActiveRecord::Base.connection()
+    result = connection.execute("SELECT DISTINCT
+                                  r.short_name as route_number,
+                                  r.long_name as route_name,
+                                  t.headsign,
+                                  stop.stop_id as stop_number
+                                  FROM services s
+                                  JOIN trips t ON t.service_id = s.service_id
+                                  JOIN routes r ON r.route_id = t.route_id
+                                  JOIN stop_times stopt ON stopt.trip_id = t.trip_id
+                                  JOIN stops stop ON stop.stop_id = stopt.stop_id
+                                  WHERE stop.stop_id = #{stop_number}
+                                  ORDER BY r.short_name")
+    result.to_a
+  end
 end
--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,4 +1,5 @@
 class Stop < ActiveRecord::Base
   has_many :stop_times, :primary_key => 'stop_id'
   has_one :parent_stop, :class_name => 'Stop', :foreign_key => 'parent_station', :primary_key => 'stop_id'
+  has_many :child_stops, :class_name => 'Stop', :foreign_key => 'parent_station', :primary_key => 'stop_id'
 end
